8cc0a0583839b0a43a4cbfcab54e6620189e6c77
Yi-Ming You <t106360241@ntut.org.tw>
2018-01-02 00:52:30 +0800

    4

--- source/main.c
+++ source/main.c
@@ -217,3 +217,59 @@ int check()
 	return(0);
 }
 
+char AI()
+{
+	int a, b;
+	char c1, c2;
+
+	srand(time(NULL));
+
+	a = rand() % 3;
+	b = rand() % 3;
+
+	switch (a)
+	{
+	case 0:
+		c1 = '1';
+		break;
+	case 1:
+		c1 = '2';
+		break;
+	case 2:
+		c1 = '3';
+		break;
+	}
+
+	switch (b)
+	{
+	case 0:
+		c2 = 'A';
+		break;
+	case 1:
+		c2 = 'B';
+		break;
+	case 2:
+		c2 = 'C';
+		break;
+	}
+
+	buf[0] = c1;
+	buf[1] = c2;
+}
+
+int AI_check(void)
+{
+	int x, y;
+
+	x = buf[0] - '1';
+	y = buf[1] - 'A';
+
+	if (a[x][y] == 1 || a[x][y] == 2)
+	{
+		return 1;
+	}
+	else if (a[x][y] == 0)
+	{
+		return 0;
+	}
+}
